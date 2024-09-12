770bfd489e51534243fe100b4b77ba215624faa0
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-09-12 16:42:59 +0200

    Implement ImagePostRepository

---
+++ nypag-spring_backend-5b58b4e2560c/src/main/java/com/example/demo/domain/imagePost/ImagePostRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.domain.imagePost;
+
+import com.example.demo.core.generic.AbstractRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImagePostRepository extends AbstractRepository<ImagePost> {
+}
