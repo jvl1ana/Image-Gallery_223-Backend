91129af2e63d6e5fcaf4af703105b53549aa154a
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-09-12 16:43:22 +0200

    Implement ImagePostMapper

---
+++ nypag-spring_backend-5b58b4e2560c/src/main/java/com/example/demo/domain/imagePost/dto/ImagePostMapper.java
@@ -0,0 +1,11 @@
+package com.example.demo.domain.imagePost.dto;
+
+import com.example.demo.core.generic.AbstractMapper;
+import com.example.demo.domain.imagePost.ImagePost;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface ImagePostMapper extends AbstractMapper<ImagePost, ImagePostDTO> {
+
+}
