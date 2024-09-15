cbd947082a1ba2491892ad18c609a8fb84628763
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-09-15 12:44:08 +0200

    Update ImagePostRepository

--- nypag-spring_backend-5b58b4e2560c/src/main/java/com/example/demo/domain/imagePost/ImagePostRepository.java
+++ nypag-spring_backend-5b58b4e2560c/src/main/java/com/example/demo/domain/imagePost/ImagePostRepository.java
@@ -3,6 +3,14 @@ package com.example.demo.domain.imagePost;
 import com.example.demo.core.generic.AbstractRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Repository
 public interface ImagePostRepository extends AbstractRepository<ImagePost> {
+    boolean existsByDescription(String description);
+    boolean existsByUrl(String url);
+
+    List<ImagePost> findAllByAuthorId(UUID authorId);
+
 }
