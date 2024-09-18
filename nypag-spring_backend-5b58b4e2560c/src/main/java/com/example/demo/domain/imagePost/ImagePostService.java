1c52843385b71c4b416706e768a51d00b4d39c50
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-09-18 23:22:11 +0200

    Implement findPostByUserId method

--- nypag-spring_backend-5b58b4e2560c/src/main/java/com/example/demo/domain/imagePost/ImagePostService.java
+++ nypag-spring_backend-5b58b4e2560c/src/main/java/com/example/demo/domain/imagePost/ImagePostService.java
@@ -2,6 +2,11 @@ package com.example.demo.domain.imagePost;
 
 import com.example.demo.core.generic.AbstractService;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface ImagePostService extends AbstractService<ImagePost> {
+
+    List<ImagePost> findPostByUserId(UUID userId);
 
 }
