a0d2c181f63ceb4e0dcd8a45cc9450b7201a755d
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-09-18 23:21:36 +0200

    Remove methods to validate if user is permitted to delete or edit a post

--- nypag-spring_backend-5b58b4e2560c/src/main/java/com/example/demo/domain/imagePost/ImagePostServiceImpl.java
+++ nypag-spring_backend-5b58b4e2560c/src/main/java/com/example/demo/domain/imagePost/ImagePostServiceImpl.java
@@ -2,12 +2,12 @@ package com.example.demo.domain.imagePost;
 
 import com.example.demo.domain.user.User;
 import com.example.demo.domain.user.UserDetailsImpl;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.webjars.NotFoundException;
 
@@ -21,36 +21,25 @@ public class ImagePostServiceImpl implements ImagePostService {
     @Autowired
     private ImagePostRepository repository;
 
+    @Transactional
     @Override
     public ImagePost save(ImagePost newImagePost) throws DataIntegrityViolationException {
-        if (repository.existsByDescription(newImagePost.getDescription()) && repository.existsByUrl(newImagePost.getUrl())) {
-            throw new DataIntegrityViolationException("An Image-Post with this description and image already exists!");
-        }
+
         return repository.save(newImagePost);
     }
 
+    @Transactional
     @Override
-    public void deleteById(UUID id) throws NoSuchElementException, AccessDeniedException {
-        ImagePost imagePost = repository.findById(id).orElseThrow(() -> new NotFoundException("Image-Post with id " + id + " couldn't be found"));
-        User currentUser = getCurrentUser();
-
-        if (!canEditOrDelete(currentUser, imagePost)) {
-            throw new AccessDeniedException("You do not have permission to delete this post.");
-        }
+    public void deleteById(UUID id) throws NoSuchElementException {
+        repository.findById(id).orElseThrow(() -> new NotFoundException("Image-Post with id " + id + " couldn't be found"));
         repository.deleteById(id);
     }
 
+    @Transactional
     @Override
-    public ImagePost updateById(UUID id, ImagePost updatedImagePost) throws NoSuchElementException, DataIntegrityViolationException, AccessDeniedException {
-        ImagePost existingPost = repository.findById(id).orElseThrow(() -> new NotFoundException("Image-Post with id " + id + " couldn't be found"));
-        User currentUser = getCurrentUser();
-        if (repository.existsByDescription(updatedImagePost.getDescription()) && repository.existsByUrl(updatedImagePost.getUrl())) {
-            throw new DataIntegrityViolationException("An Image-Post with this description and image already exists!");
-        }
+    public ImagePost updateById(UUID id, ImagePost updatedImagePost) throws NoSuchElementException, DataIntegrityViolationException {
+        repository.findById(id).orElseThrow(() -> new NotFoundException("Image-Post with id " + id + " couldn't be found"));
 
-        if (!canEditOrDelete(currentUser, existingPost)) {
-            throw new AccessDeniedException("You do not have permission to update this post.");
-        }
 
         updatedImagePost.setId(id);
         return repository.save(updatedImagePost);
@@ -77,22 +66,9 @@ public class ImagePostServiceImpl implements ImagePostService {
         return repository.existsById(id);
     }
 
+    @Override
     public List<ImagePost> findPostByUserId(UUID userId) {
         return repository.findAllByAuthorId(userId);
     }
 
-    private User getCurrentUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication.getPrincipal() instanceof UserDetailsImpl) {
-            return ((UserDetailsImpl) authentication.getPrincipal()).user();
-        }
-        throw new IllegalStateException("No user is currently authenticated");
-    }
-
-    private boolean canEditOrDelete(User user, ImagePost imagePost) {
-        if (user.getId().equals(imagePost.getAuthor().getId())) {
-            return true;
-        }
-        return user.getRoles().stream().anyMatch(role -> role.getName().equals("ADMIN"));
-    }
 }
