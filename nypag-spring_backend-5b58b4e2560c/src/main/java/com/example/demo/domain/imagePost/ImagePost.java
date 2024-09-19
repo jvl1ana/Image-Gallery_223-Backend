18a91563cabd6aa59abc9923cde425fb52c52568
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-09-19 15:39:47 +0200

    Implement lifecycle methods

--- nypag-spring_backend-5b58b4e2560c/src/main/java/com/example/demo/domain/imagePost/ImagePost.java
+++ nypag-spring_backend-5b58b4e2560c/src/main/java/com/example/demo/domain/imagePost/ImagePost.java
@@ -3,11 +3,13 @@ package com.example.demo.domain.imagePost;
 import com.example.demo.core.generic.AbstractEntity;
 import com.example.demo.domain.user.User;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import lombok.extern.log4j.Log4j2;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -18,17 +20,17 @@ import java.util.Set;
 @AllArgsConstructor
 @Getter
 @Setter
+@Log4j2
 @Accessors(chain = true)
 public class ImagePost extends AbstractEntity {
 
+    @Size(max = 10000, min = 5)
     @Column(name = "url")
     private String url;
 
+    @Size(max = 75, min = 5)
     @Column(name = "description")
     private String description;
-
-    /*@Column(name = "likes")
-    private Integer likes;*/
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "users_like", joinColumns = @JoinColumn(name = "post_id", referencedColumnName = "id"),
@@ -37,5 +39,35 @@ public class ImagePost extends AbstractEntity {
 
     @ManyToOne
     private User author;
+
+    @PrePersist
+    public void logNewImagePostAttempt(){
+        log.info("Attempting to add new image-post");
+    }
+
+    @PostPersist
+    public void logNewImagePostAdded(){
+        log.info("Added image-post");
+    }
+
+    @PreRemove
+    public void logDeleteImagePostAttempt(){
+        log.info("Attempting to delete image-post with id: " + getId());
+    }
+
+    @PostRemove
+    public void logDeleteImagePostSuccess(){
+        log.info("Image-post with id " + getId() + " successfully deleted");
+    }
+
+    @PreUpdate
+    public void logUpdateImagePostAttempt(){
+        log.info("Attempting to update image-post with id: " + getId());
+    }
+
+    @PostUpdate
+    public void logUpdateImagePostSuccess(){
+        log.info("Image-post with id " + getId() + " successfully updated");
+    }
 
 }
