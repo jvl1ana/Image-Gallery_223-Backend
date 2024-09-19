b4a3456d974cf013ef49925ed44b4aff63247801
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-09-19 15:32:48 +0200

    Update testdata

--- nypag-spring_backend-5b58b4e2560c/src/main/resources/data.sql
+++ nypag-spring_backend-5b58b4e2560c/src/main/resources/data.sql
@@ -27,10 +27,7 @@ VALUES ('2ebf301e-6c61-4076-98e3-2a38b31daf86', 'DEFAULT'),
 
 --assign roles to users
 insert into users_role (users_id, role_id)
-values ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'd29e709c-0ff1-4f4c-a7ef-09f656c390f1'),
-       ('0d8fa44c-54fd-4cd0-ace9-2a7da57992de', 'd29e709c-0ff1-4f4c-a7ef-09f656c390f1'),
-       ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'ab505c92-7280-49fd-a7de-258e618df074'),
-       ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02'),
+values ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'ab505c92-7280-49fd-a7de-258e618df074'),
        ('0d8fa44c-54fd-4cd0-ace9-2a7da57992de', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02'),
        ('3a97edf9-b481-4f33-9b4f-ef4104b15df1', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02')
     ON CONFLICT DO NOTHING;
